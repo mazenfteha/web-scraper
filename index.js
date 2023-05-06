4ad829ad4bea80226734bb1c7dec7e01b51a697e
Mazen Mohamed <104158971+mazenfteha@users.noreply.github.com>
2023-05-06 19:03:23 +0300

    Get data from backend to frontend

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const cheerio = require('cheerio')
 const axios = require('axios')
+const cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 const url = 'https://www.theguardian.com/uk'
 
-axios(url)
+app.get('/', (req,res)=>{
+    res.json('this is my webscraper')
+})
+
+app.get('/results', (req,res)=>{
+    axios(url)
     .then(response => {
         const html = response.data
         const $ = cheerio.load(html)
@@ -21,8 +28,12 @@ axios(url)
                 url: url
             })
         })
-        console.log(articles)
+        //console.log(articles)
+        res.json(articles)
     }).catch(err => console.log(err))
+})
+
+
 
 const PORT = 8000 || process.env.PORT
 app.listen(PORT, ()=>{
